5da0d999b31484f230848ff9f0c4be4438c24242
killzdesu <killzdesu@gmail.com>
2022-06-25 18:49:27 +0700

    update latest dotfiles

--- vim/after/plugin/lsp-config.conf.lua
+++ vim/after/plugin/lsp-config.conf.lua
@@ -49,7 +49,62 @@ for _, lsp in pairs(servers) do
   }
 end
 
+nvim_lsp.rust_analyzer.setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "rustup run nightly rust-analyzer" },
+  flags = {
+    debounce_text_changes = 150,
+  },
+  settings = {
+    ["rust-analyzer"] = {
+      assist = {
+        importGranularity = "module",
+        importPrefix = "self",
+      },
+      cargo = {
+        loadOutDirsFromCheck = true
+      },
+      procMacro = {
+        enable = true
+      },
+      checkOnSave = {
+        command = "clippy"
+      },
+    }
+  }
+}
 
+local opts = {
+  tools = { -- rust-tools options
+    autoSetHints = true,
+    hover_with_actions = true,
+    inlay_hints = {
+      show_parameter_hints = false,
+    parameter_hints_prefix = "",
+    other_hints_prefix = "",
+    },
+  },
+  -- all the opts to send to nvim-lspconfig
+  -- these override the defaults set by rust-tools.nvim
+  -- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
+  server = {
+    -- on_attach is a callback called when the language server attachs to the buffer
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+      -- to enable rust-analyzer settings visit:
+      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+      ["rust-analyzer"] = {
+        -- enable clippy on save
+        checkOnSave = {
+          command = "clippy"
+        },
+      }
+    }
+  },
+}
+require('rust-tools').setup(opts)
 
 -------- Volar --------
 
